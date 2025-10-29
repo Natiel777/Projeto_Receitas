471ce24821daa9e4d3caab13224196b7063bc271
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-10-29 19:39:00 -0300

    Fetch users from API and display in components

--- Frontend/frontend-react/src/App.jsx
+++ Frontend/frontend-react/src/App.jsx
@@ -1,1 +1,24 @@
+import { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
+import "./App.css";
 
+function App() {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/usuarios")
+      .then(res => res.json())
+      .then(setUsuarios);
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Receitas - React</h1>
+      <Formulario setUsuarios={setUsuarios} />
+      <Lista usuarios={usuarios} />
+    </div>
+  );
+}
+
+export default App;
