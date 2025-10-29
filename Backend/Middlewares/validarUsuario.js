73a064596f6f325b0faa116dc99026b2b3b56a0c
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-10-29 19:36:11 -0300

    Add user validation middleware

--- Backend/Middlewares/validarUsuario.js
+++ Backend/Middlewares/validarUsuario.js
@@ -1,1 +1,7 @@
-
+export const validarUsuario = (req, res, next) => {
+  const { nome, email, senha } = req.body;
+  if (!nome || !email || !senha) {
+    return res.status(400).json({ erro: "Campos obrigatórios ausentes" });
+  }
+  next();
+};
