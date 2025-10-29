4e3feea1445fbce63cf1e125f136209a546132c2
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-10-29 20:05:18 -0300

    Rename usuariosRoute.js to usuarioRoutes.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import usuariosRouter from "./Routes/usuariosRoute.js";
+import usuariosRouter from "./Routes/usuarioRoutes.js";
 import { logger } from "./Middlewares/logger.js";
 import { tratarErros } from "./Middlewares/tratarErros.js";
 
