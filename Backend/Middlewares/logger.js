d1f17c4e72a024b216404e138d02479ee1ebbfb9
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-10-30 15:53:12 -0300

    Refactor logger middleware to use function declaration

--- Backend/Middlewares/logger.js
+++ Backend/Middlewares/logger.js
@@ -1,4 +1,4 @@
-export const logger = (req, res, next) => {
-  console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
+export function logger(req, res, next) {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
   next();
-};
+}
