52de543dc2cdca813767d15798318519a18a143f
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-10-29 21:13:42 -0300

    Atualizar o RecipeCard.jsx

--- Frontend/frontend-react/src/components/RecipeCard.jsx
+++ Frontend/frontend-react/src/components/RecipeCard.jsx
@@ -1,1 +1,12 @@
-.
+import React from "react";
+
+function RecipeCard({ receita }) {
+  return (
+    <div className="recipe-card">
+      <h2>{receita.nome}</h2>
+      <p>{receita.descricao}</p>
+    </div>
+  );
+}
+
+export default RecipeCard;
