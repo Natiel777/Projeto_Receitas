46bb97c14dbc5913d9d295d5d9ef0c48ff05e90d
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-02 17:21:00 -0300

    (fix) Atualizar o db.js

--- Backend/db.js
+++ Backend/db.js
@@ -6,19 +6,30 @@ const db = await open({
   driver: sqlite3.Database
 });
 
+// Criação das tabelas
 await db.exec(`
 CREATE TABLE IF NOT EXISTS usuarios (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  nome TEXT, email TEXT UNIQUE, senha TEXT
+  nome TEXT, 
+  email TEXT UNIQUE, 
+  senha TEXT
 );
+
 CREATE TABLE IF NOT EXISTS receitas (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  titulo TEXT, ingredientes TEXT, modo_preparo TEXT, imagem TEXT, autor_id INTEGER,
+  titulo TEXT, 
+  ingredientes TEXT, 
+  modo_preparo TEXT, 
+  imagem TEXT, 
+  autor_id INTEGER,
   FOREIGN KEY (autor_id) REFERENCES usuarios(id)
 );
+
 CREATE TABLE IF NOT EXISTS avaliacoes (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  receita_id INTEGER, usuario_id INTEGER, nota INTEGER,
+  receita_id INTEGER, 
+  usuario_id INTEGER, 
+  nota INTEGER,
   FOREIGN KEY (receita_id) REFERENCES receitas(id),
   FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
