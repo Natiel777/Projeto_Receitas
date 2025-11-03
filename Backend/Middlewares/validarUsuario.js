d6f2266d4479e0da3f5eaa294aae2afdc99be016
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-03 17:27:39 -0300

    Delete Backend/Middlewares/validarUsuario.js

--- Backend/Middlewares/validarUsuario.js
+++
@@ -1,5 +0,0 @@
-export function validarUsuario(req, res, next) {
-  const { nome, email, senha } = req.body;
-  if (!nome || !email || !senha) return res.status(400).json({ erro: 'Campos obrigatórios' });
-  next();
-}
