de6c6cccac8ee05a988eb1041ce204a166d1ae26
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-17 21:57:44 -0300

    Delete Backend directory

--- Backend/Models/Avaliacao.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "../db.js";
-
-const AvaliacaoSchema = new mongoose.Schema({
-  receita_id: { type: mongoose.Schema.Types.ObjectId, ref: "Receita" },
-  usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: "Usuario" },
-  nota: Number,
-});
-
-export default mongoose.model("Avaliacao", AvaliacaoSchema);
