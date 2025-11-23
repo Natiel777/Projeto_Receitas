ea8e3c61cbb77d6b52e4cb3dbbb0dfc9c4712d8d
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-23 19:35:56 -0300

    Delete backend directory

--- backend/src/middlewares/tratarErros.js
+++
@@ -1,6 +0,0 @@
-const tratarErros = (err, req, res, next) => {
-  console.error("Erro:", err.message || err);
-  res.status(500).json({ erro: "Erro do Servidor Interno" });
-};
-
-module.exports = tratarErros;
