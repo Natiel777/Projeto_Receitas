81cd3b4d25bb0bdb2256830cc7bf45dccb472f02
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-24 18:43:34 -0300

    Delete suareceita directory

--- suareceita/backend/src/middlewares/errors.js
+++
@@ -1,6 +0,0 @@
-const errors = (err, req, res, next) => {
-  console.error("Erro:", err.message || err);
-  res.status(500).json({ erro: "Erro do Servidor Interno" });
-};
-
-module.exports = errors;
