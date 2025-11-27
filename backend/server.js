25b80b2baa3eb3659030f27bdac55ff8d7be5d19
Natiel777 <natiel.silva11@aluno.ifce.edu.br>
2025-11-26 21:20:49 -0300

    Update CORS origin for server configuration

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "https://suareceita.onrender.com" }));
+app.use(cors({ origin: "https://suareceita-wyze.onrender.com" }));
 app.use(express.json());
 app.use(cookieParser());
 
