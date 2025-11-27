5f1b778f8538d538b51c54c42b19e8cfe9ca6ea2
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-27 17:33:15 -0300

    Atualizar o CardRecipe.jsx

--- frontend/src/components/CardRecipe.jsx
+++ frontend/src/components/CardRecipe.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { BASE_SERVER_URL } from "../services/api";
 
 function CardReceita({ receita }) {
   const nomeDoAutor = receita.usuario?.nome || "Autor Desconhecido";
@@ -6,17 +7,17 @@ function CardReceita({ receita }) {
   return (
     <div className="border rounded-lg shadow-md p-4 flex flex-col">
       <img
-        src={`https://suareceitaserver/uploads/${receita.imagem}`}
+        src={`${BASE_SERVER_URL}/uploads/${receita.imagem}`}
         alt={receita.titulo}
         className="w-full h-40 sm:h-48 object-cover rounded mb-3"
       />
-      
+
       <p className="text-xs text-orange-500 dark:text-white mb-1">
         Por: <span className="font-semibold">{nomeDoAutor}</span>
       </p>
 
       <h2 className="text-lg font-bold mb-2 truncate">{receita.titulo}</h2>
-      
+
       <p className="text-sm text-black flex-grow line-clamp-3 dark:text-white">
         {receita.descricao}
       </p>
