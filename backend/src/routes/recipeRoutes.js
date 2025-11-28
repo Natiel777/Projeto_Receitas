dfc8bf3d9727122a090527d8930d21835c9131f8
Herbert <francisco.xavier09@aluno.ifce.edu.br>
2025-11-27 21:22:23 -0300

    Fix: Atualizar o recipeRoutes.js

--- backend/src/routes/recipeRoutes.js
+++ backend/src/routes/recipeRoutes.js
@@ -2,24 +2,22 @@ const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 const auth = require("../middlewares/auth");
-const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
-});
-
-const upload = multer({ storage });
+const upload = require("../middlewares/cloudinaryConfig"); 
+// A linha 'const multer = require("multer");' e a config 'storage' foram removidas.
 
 router.post("/", auth, upload.single("imagem"), recipeController.criar);
+
 router.get("/", recipeController.listar);
 router.get("/:id", recipeController.detalhes);
+
 router.put(
   "/:id",
   auth,
   upload.single("imagem"),
   recipeController.editar
 );
+
 router.delete("/:id", auth, recipeController.excluir);
 
 module.exports = router;
